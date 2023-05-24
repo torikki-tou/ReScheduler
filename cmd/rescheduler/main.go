373d7808e2d56a8bd4fc8d9b87801d432eff52f5
torikki <torikki@protonmail.com>
2023-05-24 23:22:21 +0300

    Created dependency injection core

--- cmd/rescheduler/main.go
+++ cmd/rescheduler/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"github.com/torikki-tou/ReScheduler/internal/api"
+	v1Api "github.com/torikki-tou/ReScheduler/internal/api/v1"
+	taskV1Api "github.com/torikki-tou/ReScheduler/internal/api/v1/api/task"
+	configComponent "github.com/torikki-tou/ReScheduler/internal/components/config"
+	serverComponent "github.com/torikki-tou/ReScheduler/internal/components/server"
+	"go.uber.org/fx"
+)
+
 func main() {
-	println("Hello world")
+	fx.New(
+		fx.Provide(
+			func() *configComponent.Config {
+				config := configComponent.New()
+				config.SetDefaults()
+				return config
+			},
+			func() *taskV1Api.API {
+				return taskV1Api.New()
+			},
+			func(task *taskV1Api.API) *v1Api.API {
+				return v1Api.New(task)
+			},
+			func(v1 *v1Api.API) *api.API {
+				return api.New(v1)
+			},
+			func(api *api.API, config *configComponent.Config) *serverComponent.Server {
+				return serverComponent.New(api.Router(), config)
+			},
+		),
+		fx.Invoke(
+			func(server *serverComponent.Server) {
+				_ = server.Run()
+			},
+		),
+	)
 }
