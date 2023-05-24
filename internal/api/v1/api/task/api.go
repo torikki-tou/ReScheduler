d062a199f20faddff86315b132e02d5b5cc80df6
torikki <torikki@protonmail.com>
2023-05-24 23:29:55 +0300

    Included task service into task api

--- internal/api/v1/api/task/api.go
+++ internal/api/v1/api/task/api.go
@@ -1,12 +1,13 @@
 package task
 
 import (
-	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/torikki-tou/ReScheduler/internal/services/task"
 	"net/http"
 )
 
 type API struct {
+	service *task.Service
 }
 
 func New() *API {
@@ -14,13 +15,32 @@ func New() *API {
 }
 
 func (a *API) Get(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte(fmt.Sprint("This is a task ") + r.RequestURI))
+	_, _ = w.Write([]byte(a.service.Get() + " " + chi.URLParam(r, "id")))
 }
 
+func (a *API) Search(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte(a.service.Search()))
+}
+
+func (a *API) Create(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte(a.service.Create()))
+}
+
+func (a *API) Update(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte(a.service.Update() + " " + chi.URLParam(r, "id")))
+}
+
+func (a *API) Delete(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte(a.service.Delete() + " " + chi.URLParam(r, "id")))
+}
 func (a *API) Router() *chi.Mux {
 	router := chi.NewRouter()
 
-	router.Get("/", a.Get)
+	router.Get("/", a.Search)
+	router.Get("/{id}", a.Get)
+	router.Post("/", a.Create)
+	router.Patch("/{id}", a.Update)
+	router.Delete("/{id}", a.Delete)
 
 	return router
 }
