8728042fabc051488789dd948b11138f98c4a642
torikki <torikki@protonmail.com>
2023-05-25 23:07:33 +0300

    Created DTO package for task service

---
+++ internal/services/task/dto/create.go
@@ -0,0 +1,15 @@
+package dto
+
+import "encoding/json"
+
+type (
+	CreateRequest struct {
+		ID             *string
+		CronExpression string
+		Message        json.RawMessage
+	}
+
+	CreateResponse struct {
+		Task Task
+	}
+)
