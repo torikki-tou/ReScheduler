30dcca1754596a536f1dac6d7f742175b5c055f0
torikki <torikki@protonmail.com>
2023-05-27 13:52:16 +0300

    Provided task repository to task service

--- internal/services/task/service.go
+++ internal/services/task/service.go
@@ -1,24 +1,71 @@
 package task
 
-import "github.com/torikki-tou/ReScheduler/internal/services/task/dto"
+import (
+	taskRepository "github.com/torikki-tou/ReScheduler/internal/repositories/task"
+	repositoryDto "github.com/torikki-tou/ReScheduler/internal/repositories/task/dto"
+	"github.com/torikki-tou/ReScheduler/internal/services/task/dto"
+)
 
 type Service struct {
+	repository taskRepository.Repository
 }
 
-func New() *Service {
-	return &Service{}
+func New(repository taskRepository.Repository) *Service {
+	return &Service{
+		repository: repository,
+	}
 }
 
 func (s *Service) Get(req *dto.GetRequest) *dto.GetResponse {
-	return &dto.GetResponse{}
+
+	res := s.repository.Get(&repositoryDto.GetRequest{ID: req.ID})
+
+	task := res.Task
+
+	return &dto.GetResponse{Task: dto.Task{
+		ID:             task.ID,
+		CronExpression: task.CronExpression,
+		Message:        task.Message,
+	}}
 }
 
 func (s *Service) Search(req *dto.SearchRequest) *dto.SearchResponse {
-	return &dto.SearchResponse{}
+	res := s.repository.Search(&repositoryDto.SearchRequest{Limit: req.Limit})
+
+	var tasks = make([]dto.Task, 0, len(res.Tasks))
+	for _, task := range res.Tasks {
+		tasks = append(tasks, dto.Task{
+			ID:             task.ID,
+			CronExpression: task.CronExpression,
+			Message:        task.Message,
+		})
+	}
+
+	return &dto.SearchResponse{Tasks: tasks}
 }
 
 func (s *Service) Create(req *dto.CreateRequest) *dto.CreateResponse {
-	return &dto.CreateResponse{}
+
+	var id string
+	if req.ID != nil {
+		id = *req.ID
+	} else {
+		id = "1"
+	}
+
+	res := s.repository.Create(&repositoryDto.CreateRequest{
+		ID:             id,
+		CronExpression: req.CronExpression,
+		Message:        req.Message,
+	})
+
+	task := res.Task
+
+	return &dto.CreateResponse{Task: dto.Task{
+		ID:             task.ID,
+		CronExpression: task.CronExpression,
+		Message:        task.Message,
+	}}
 }
 
 func (s *Service) Update(req *dto.UpdateRequest) *dto.UpdateResponse {
@@ -26,5 +73,14 @@ func (s *Service) Update(req *dto.UpdateRequest) *dto.UpdateResponse {
 }
 
 func (s *Service) Delete(req *dto.DeleteRequest) *dto.DeleteResponse {
-	return &dto.DeleteResponse{}
+
+	res := s.repository.Delete(&repositoryDto.DeleteRequest{ID: req.ID})
+
+	task := res.Task
+
+	return &dto.DeleteResponse{Task: dto.Task{
+		ID:             task.ID,
+		CronExpression: task.CronExpression,
+		Message:        task.Message,
+	}}
 }
