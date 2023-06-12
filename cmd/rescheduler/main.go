26b15ff0dce44e240d826c8e4005e270ebd1df89
torikki <torikki@protonmail.com>
2023-06-12 22:19:02 +0300

    Added mechanism to choose storage type

--- cmd/rescheduler/main.go
+++ cmd/rescheduler/main.go
@@ -6,7 +6,9 @@ import (
 	taskV1Api "github.com/torikki-tou/ReScheduler/internal/api/v1/api/task"
 	configComponent "github.com/torikki-tou/ReScheduler/internal/components/config"
 	serverComponent "github.com/torikki-tou/ReScheduler/internal/components/server"
-	taskRepository "github.com/torikki-tou/ReScheduler/internal/repositories/task/memory"
+	taskRepository "github.com/torikki-tou/ReScheduler/internal/repositories/task"
+	taskRepositoryMemory "github.com/torikki-tou/ReScheduler/internal/repositories/task/memory"
+	taskRepositoryRedis "github.com/torikki-tou/ReScheduler/internal/repositories/task/redis"
 	taskService "github.com/torikki-tou/ReScheduler/internal/services/task"
 	"go.uber.org/fx"
 )
@@ -19,10 +21,17 @@ func main() {
 				config.SetDefaults()
 				return config
 			},
-			func() *taskRepository.Repository {
-				return taskRepository.New()
+			func(config *configComponent.Config) taskRepository.Repository {
+				switch config.GetStorageType() {
+				case configComponent.StorageTypeRedis:
+					return taskRepositoryRedis.New(config)
+				case configComponent.StorageTypeMemory:
+					return taskRepositoryMemory.New()
+				default:
+					return nil
+				}
 			},
-			func(repository *taskRepository.Repository) *taskService.Service {
+			func(repository taskRepository.Repository) *taskService.Service {
 				return taskService.New(repository)
 			},
 			func(service *taskService.Service) *taskV1Api.API {
--- internal/components/config/component.go
+++ internal/components/config/component.go
@@ -2,6 +2,11 @@ package config
 
 import "github.com/spf13/viper"
 
+const (
+	StorageTypeRedis  = `redis`
+	StorageTypeMemory = `memory`
+)
+
 type Config struct {
 	config *viper.Viper
 }
@@ -16,6 +21,7 @@ func (c *Config) SetDefaults() {
 	c.config.SetDefault("app_port", 3000)
 	c.config.SetDefault("redis_port", 6379)
 	c.config.SetDefault("redis_host", "redis")
+	c.config.SetDefault("storage_type", StorageTypeRedis)
 }
 
 func (c *Config) AppPort() int {
@@ -28,4 +34,8 @@ func (c *Config) RedisPort() int {
 
 func (c *Config) RedisHost() string {
 	return c.config.GetString("redis_host")
+}
+
+func (c *Config) GetStorageType() string {
+	return c.config.GetString("storage_type")
 }
